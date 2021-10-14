2160f0a7a6c8474e6f9500b675ff636c04bf456a
gergő szijártó <gergo.szijarto@hotmail.com>
2021-10-14 21:00:33 +0200

    added script for entry site

---
+++ entryscript.js
@@ -0,0 +1,19 @@
+var timesclicked = 0;
+const button = document.getElementById("button");
+var green = 0;
+
+$("#button").click(function() {
+    button.style.color = "rgb(0," + (green + 255) + ",0)";
+    green = green + 255;
+
+    timesclicked = timesclicked + 1;
+
+    if (timesclicked == 1) {
+        // change cursor maybe idk
+    } else if (timesclicked == 2) {
+        window.open("home.html", "_self");
+
+    }
+
+
+});
